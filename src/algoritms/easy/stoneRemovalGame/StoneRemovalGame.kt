cab42c8c24ad0ded6641d7894256a9521a648f09
m_arghavani <m_arghavani@shatel.ir>
2025-04-13 10:48:49 +0330

    stone remove solved

--- src/algoritms/easy/stoneRemovalGame/StoneRemovalGame.kt
+++ src/algoritms/easy/stoneRemovalGame/StoneRemovalGame.kt
@@ -12,19 +12,43 @@ class Solution {
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+class Solution2 {
+    fun aliceWins (n:Int) : Boolean {
+        if (n < 10) return false
+        var pickNumber = 10
+        var round = 0
+        var remainS = n
+        while ( remainS - (pickNumber - round) > (pickNumber - round-1)) {
+            round++
+        }
+        return  round%2 == 0
+    }
+}
+
+
 fun main() {
-    PerformanceTester.measureExecution(1000) { Solution().aliceWins(100) }
+    PerformanceTester.measureExecution(1000) { Solution().aliceWins(12) }
 
     val tests = listOf(
         100 to true,
         50 to true,
         30 to true,
-        25 to false,
-        75 to false
+        1 to false,
+        12 to true
     )
 
     for ((input, expected) in tests) {
-        val result = Solution().aliceWins(input)
+        val result = Solution2().aliceWins(input)
         println("Input: n = $input => Output: $result (Expected: $expected)")
     }
 }
